1882a27dc8d1fd10fda21a8117eb8d64c5340693
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-19 00:00:30 +0700

    Update git.py

--- AyiinXd/core/git.py
+++ AyiinXd/core/git.py
@@ -30,7 +30,7 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
 def git():
     UPSTREAM_REPO = b64decode(
-        "aHR0cHM6Ly9naXRodWIuY29tL2lvbm11c2ljL0thenUtVXNlcmJvdA=="
+        "aHR0cHM6Ly9naXRodWIuY29tL05vdEx1bWllcmUvTHVtaWVyZS11Ym90"
     ).decode("utf-8")
     try:
         repo = Repo()
@@ -61,4 +61,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGS.info("Fetched Updates from Uputt-Userbot")
+        LOGS.info("Fetched Updates from Lumiere-Userbot")
