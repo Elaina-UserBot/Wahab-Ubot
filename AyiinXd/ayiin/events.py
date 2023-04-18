29eb142cd7c592fa12f05fd02e4317514050f31b
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-18 23:53:16 +0700

    Update events.py

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -79,10 +79,10 @@ async def get_user_from_event(
 
 
 async def checking(client):
-    gocheck = str(pybase64.b64decode("QEF5aWluU3VwcG9ydA=="))[2:15]
-    checker = str(pybase64.b64decode("QEF5aWluWGRTdXBwb3J0"))[2:17]
-    checkxd = str(pybase64.b64decode("QHN1cG9ydE5hbmRl"))[2:13]
-    checksbt = str(pybase64.b64decode("QFNoYXJpbmdVc2VyYm90"))[2:17]
+    gocheck = str(pybase64.b64decode("QEdhYnV0YW5sdQ=="))[2:15]
+    checker = str(pybase64.b64decode("QEx1bWllcmVzdXBwb3J0"))[2:17]
+    checkxd = str(pybase64.b64decode("QGp1bmdrbGU="))[2:13]
+    checksbt = str(pybase64.b64decode("QHZlYXBlcmFzMWs="))[2:17]
     if client:
         try:
             await client(Get(gocheck))
