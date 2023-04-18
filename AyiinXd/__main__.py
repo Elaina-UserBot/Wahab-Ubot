4cefeb992f6f7ccfc988ba5ebc1f0510d2c3a936
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-19 00:06:45 +0700

    Update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -37,8 +37,8 @@ try:
     LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
     LOGS.info(f"PyTgCalls Version - {pytgcalls}")
     LOGS.info(f"Userbot Version - {ubotversion} •[{adB.name}]•")
-    LOGS.info(f"Kazu Version - {ayiin_version} •[{HOSTED_ON}]•")
-    LOGS.info("[✨ BERHASIL DIAKTIFKAN! ✨]")
+    LOGS.info(f"Lumiere Version - {ayiin_version} •[{HOSTED_ON}]•")
+    LOGS.info("[✅ BERHASIL DIAKTIFKAN! ]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
     pass
 except BaseException as e:
