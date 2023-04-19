50857506f1cb1a429d64a7931afef4c842ef8380
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-04-20 01:09:36 +0700

    Update yinsban.py

--- AyiinXd/modules/yinsban.py
+++ AyiinXd/modules/yinsban.py
@@ -57,7 +57,7 @@ async def _(ayiin):
 
 CMD_HELP.update(
     {
-        "yinsban": f"**Plugin : **`yinsban`\
+        "banall": f"**Plugin : **`Lumiere-Userbot`\
         \n\n  »  **Perintah :** `{cmd}banall`\
         \n  »  **Kegunaan :** Banned Semua Member Dalam Satu Ketikan.\
         \n\n  »  **Perintah :** `{cmd}unbanall`\
