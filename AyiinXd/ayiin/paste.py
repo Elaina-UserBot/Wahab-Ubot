44fc35ec9a7a1895560d588770637220f28dee6d
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-01 13:53:49 +0700

    Update paste.py

--- AyiinXd/ayiin/paste.py
+++ AyiinXd/ayiin/paste.py
@@ -5,7 +5,7 @@ import requests
 from AyiinXd import BOTLOG_CHATID
 from AyiinXd.ayiin.logger import logging
 
-LOGS = logging.getLogger("AyiinUserBot")
+LOGS = logging.getLogger("LumiereUserbot")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
