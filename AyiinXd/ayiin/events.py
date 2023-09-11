0856afacd2fad2cab4ff11c47416d41f16eeb418
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-11 22:54:53 +0700

    Update events.py

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -81,7 +81,7 @@ async def get_user_from_event(
 
 
 async def checking(client):
-    join = ["Gabutanlu", "Lumieresupport", "LumiereProject", " Syavenstore"]
+    join = ["Gabutanlu", "Lumieresupport", "LumiereProject", " PesulapTelegram"]
     for a in join:
         try:
             await client(Get(a))
