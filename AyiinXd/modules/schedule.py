82fd74d858dc0f6cd07c01247eec5cb8eb0a8c35
NotLumiere <118038685+NotLumiere@users.noreply.github.com>
2023-09-17 11:31:49 +0700

    Update schedule.py

--- AyiinXd/modules/schedule.py
+++ AyiinXd/modules/schedule.py
@@ -33,37 +33,37 @@ def ban_time(time_str):
     return to_return
   
 @ayiin_cmd(pattern="schedule( (.*)|$)", fullsudo=True)
-async def _(e):
-    x = e.pattern_match.group(1).strip()
-    xx = await e.get_reply_message()
+async def schedule(event):
+    x = event.pattern_match.group(1).strip()
+    xx = await event.get_reply_message()
     if x and not xx:
         y = x.split(" ")[-1]
         k = x.replace(y, "")
         if y.isdigit():
-            await e.client.send_message(
-                e.chat_id, k, schedule=timedelta(seconds=int(y))
+            await event.client.send_message(
+                event.chat_id, k, schedule=timedelta(seconds=int(y))
             )
-            await e.eor(get_string("schdl_1"), time=5)
+            await event.eor(get_string("schdl_1"), time=5)
         else:
             try:
                 z = ban_time(y)
-                await e.respond(k, schedule=z)
-                await e.eor(get_string("schdl_1"), time=5)
+                await event.respond(k, schedule=z)
+                await event.eor(get_string("schdl_1"), time=5)
             except BaseException:
-                await e.eor(get_string("schdl_2"), time=5)
+                await event.eor(get_string("schdl_2"), time=5)
     elif xx and x:
         if x.isdigit():
-            await e.respond(xx, schedule=timedelta(seconds=int(x)))
-            await e.eor(get_string("schdl_1"), time=5)
+            await event.respond(xx, schedule=timedelta(seconds=int(x)))
+            await event.eor(get_string("schdl_1"), time=5)
         else:
             try:
                 z = ban_time(x)
-                await e.respond(xx, schedule=z)
-                await e.eor(get_string("schdl_1"), time=5)
+                await event.respond(xx, schedule=z)
+                await event.eor(get_string("schdl_1"), time=5)
             except BaseException:
-                await e.eor(get_string("schdl_2"), time=5)
+                await event.eor(get_string("schdl_2"), time=5)
     else:
-        return await e.eor(get_string("schdl_2"), time=5)
+        return await event.eor(get_string("schdl_2"), time=5)
 
 CMD_HELP.update(
     {
